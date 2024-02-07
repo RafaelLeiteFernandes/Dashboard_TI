85d9319a474d74fa9a5ba2269cd551fe217cec9a
rafaelleitefernandes <rafael.fernandesAIMEC@gmail.com>
2024-02-07 20:24:16 -0300

    controller fechados essa semana

---
+++ src/controllers/CloseWeekTickets.controller.ts
@@ -0,0 +1,29 @@
+// FechadosNaSemanaController.ts
+import { Request, Response } from 'express';
+import { Tickets } from '../models/Tickets';
+import { Sequelize, Op } from 'sequelize';
+
+export const getSumOfTicketsClosedThisWeek = async (req: Request, res: Response) => {
+  try {
+    // Determinar o período da semana atual
+    const now = new Date();
+    const firstDayOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay());
+    const lastDayOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay() + 6);
+
+    // Consultar o banco de dados para buscar os tickets fechados dentro desse período
+    const countOfClosedTickets = await Tickets.count({
+      where: {
+        status: 6, // Supondo que o status 6 representa tickets fechados
+        closedate: {
+          [Op.between]: [firstDayOfWeek, lastDayOfWeek]
+        }
+      }
+    });
+
+    // Retornar o resultado como resposta da requisição
+    res.json({ countOfClosedTickets });
+  } catch (error) {
+    console.error('Erro ao calcular a soma de tickets fechados nesta semana:', error);
+    res.status(500).json({ error: 'Erro ao calcular a soma de tickets fechados nesta semana.' });
+  }
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import * as TicketsController from '../controllers/AllTickets.controller';
 import * as AllTickets from '../controllers/SumAllTickets.controller'
+import * as CloseWeek from '../controllers/CloseWeekTickets.controller'
+
 
 
 const routes = Router();
 
 routes.get('/tickets', TicketsController.getAllTickets);
-routes.get('/alltickets',AllTickets.getSumOfTicketsClosed)
+routes.get('/alltickets',AllTickets.getSumOfTicketsClosed);
+routes.get('/ticketsweek', CloseWeek.getSumOfTicketsClosedThisWeek);
 
 export  {routes};
 
